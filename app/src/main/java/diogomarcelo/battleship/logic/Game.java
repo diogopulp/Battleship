c52e150e5cd1ad2d2c4b1fcca599b4d01dd4f0bd
diogomsantos <diogosantos@easyclick.pt>
2018-09-03 23:12:49 +0100

    game logic implementation started

---
+++ app/src/main/java/diogomarcelo/battleship/logic/Game.java
@@ -0,0 +1,29 @@
+package diogomarcelo.battleship.logic;
+
+public class Game {
+
+    Board boardP1, boardP2;
+    Player p1, p2;
+
+    public Game(int gameType){
+
+        // Human VS CPU
+        if (gameType == 1){
+
+
+
+        }
+        // Human VS Human
+        else{
+
+        }
+
+    }
+
+    public void setupPieces(Board board){
+
+        //board.addShip();
+        
+    }
+
+}
